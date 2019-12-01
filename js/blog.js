e5219435071fb6fcee322c1521970bb27eefe72a
Alex Eastman <alexeast@buffalo.edu>
2019-12-01 18:43:33 -0500

    Restructured projects page. Will structure other non-home pages to follow. Adding scrollmagic and gsap animations as well.

---
+++ js/blog.js
@@ -0,0 +1,12 @@
+let controller = new ScrollMagic.Controller();
+
+let fadeAway0 = gsap.to("#p0", {opacity: 0});
+
+let scene0 = new ScrollMagic.Scene({
+  triggerElement: "#p0-trigger",
+  duration: $(window).height()
+})
+  .setTween(fadeAway0)
+  .setPin("#p0");
+
+controller.addScene(scene0);
