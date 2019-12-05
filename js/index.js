d9657400d60ddae040a15edfc62866d1498e28b2
Alex Eastman <alexeast@buffalo.edu>
2019-12-04 20:55:59 -0500

    Increased height of screenimg to cover entire screen, exlcuding header row. Adjusted duration of fade on screenimg. Set pin on first row of tiles, as well as animation to make the first row's anchor element opaque when the second row covers them. First row will stay pinned to middle of screen indefinitely, eventually being covered by the second row of tiles. Also removed the bottom row class on the bottom row, as it was interfering with scroll magic.

--- js/index.js
+++ js/index.js
@@ -12,11 +12,34 @@ if (!document.cookie) {
     .from("#name", {opacity: 0, delay: .7})
     .from("#developer", {opacity: 0, delay: .2})
     .from("#engineer", {opacity: 0})
-    .from("#c0", {opacity: 0}, 3.3)
-    .from("#c1", {opacity: 0}, 3.3)
-    .from("#c2", {opacity: 0}, 3.3)
-    .from("#c3", {opacity: 0}, 3.3)
-    .from("#c4", {opacity: 0}, 3.3)
-    .from("#c5", {opacity: 0}, 3.3);
+    .from("#screenimg", {opacity: 0}, 3.3);
 
 }
+
+let bye = gsap.to(".sl-d", {opacity: 0, duration: .25});
+
+let disappear = new gsap.timeline({})
+  .to("#screenimg", {opacity: 0}, 0);
+
+let controller = new ScrollMagic.Controller();
+
+let scene = new ScrollMagic.Scene({
+  triggerHook: "onEnter",
+  duration: $(window).height()
+})
+  .setTween(disappear)
+  .addTo(controller);
+
+let scene2 = new ScrollMagic.Scene({
+  triggerHook: 0,
+  triggerElement: "#firstRow"
+})
+  .setPin("#firstRow", {pushFollowers: false})
+  .addTo(controller);
+
+let scene3 = new ScrollMagic.Scene({
+  triggerHook: .1,
+  triggerElement: "#secondRow"
+})
+  .setTween(bye)
+  .addTo(controller);
