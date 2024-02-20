fe2131fae6a06122cfcb6755e8fd30e802c7ea85
Alex Eastman <alexeast@buffalo.edu>
2024-02-19 20:30:00 -0500

    wip card

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const Center());
+void main() => runApp(const Website());
+
+class Website extends StatelessWidget {
+  const Website({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Card(child: Text("Work in progress!")),);
+  }
+}
